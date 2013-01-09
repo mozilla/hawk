f144c5d299b8465834e35dce16db4b94b3f11893
Eran Hammer <eran@hueniverse.com>
2013-01-09 08:02:18 -0800

    Add tests for normalized string

--- test/crypto.js
+++ test/crypto.js
@@ -26,13 +26,51 @@ describe('Hawk', function () {
                     header: 'core',
                     key: 'dasdfasdf',
                     algorithm: 'hmac-sha-0',
-                    timestamp: Date.now() / 1000,
+                    timestamp: Math.floor(Date.now() / 1000),
                     nonce: 'k3k4j5',
                     method: 'GET',
                     uri: '/resource/something',
                     host: 'example.com',
                     port: 8080
                 })).to.equal('');
+
+                done();
+            });
+        });
+
+        describe('#generateNormalizedString', function () {
+
+            it('should return a valid normalized string', function (done) {
+
+                expect(Hawk.crypto.generateNormalizedString({
+                    header: 'core',
+                    key: 'dasdfasdf',
+                    algorithm: 'hmac-sha-256',
+                    timestamp: 1357747017,
+                    nonce: 'k3k4j5',
+                    method: 'GET',
+                    uri: '/resource/something',
+                    host: 'example.com',
+                    port: 8080
+                })).to.equal('core.1\n1357747017\nk3k4j5\nGET\n/resource/something\nexample.com\n8080\n\n');
+
+                done();
+            });
+
+            it('should return a valid normalized string (ext)', function (done) {
+
+                expect(Hawk.crypto.generateNormalizedString({
+                    header: 'core',
+                    key: 'dasdfasdf',
+                    algorithm: 'hmac-sha-256',
+                    timestamp: 1357747017,
+                    nonce: 'k3k4j5',
+                    method: 'GET',
+                    uri: '/resource/something',
+                    host: 'example.com',
+                    port: 8080,
+                    ext: 'this is some app data'
+                })).to.equal('core.1\n1357747017\nk3k4j5\nGET\n/resource/something\nexample.com\n8080\nthis is some app data\n');
 
                 done();
             });
