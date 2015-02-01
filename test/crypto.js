86cf598c9eed629bf723aafb3e32eed0d7862678
Yury Korolev <yurykorolev@me.com>
2015-02-01 22:47:02 +0300

    Removed unused params

--- test/crypto.js
+++ test/crypto.js
@@ -27,10 +27,6 @@ describe('Hawk', function () {
             it('should return a valid normalized string', function (done) {
 
                 expect(Hawk.crypto.generateNormalizedString('header', {
-                    credentials: {
-                        key: 'dasdfasdf',
-                        algorithm: 'sha256'
-                    },
                     ts: 1357747017,
                     nonce: 'k3k4j5',
                     method: 'GET',
@@ -45,10 +41,6 @@ describe('Hawk', function () {
             it('should return a valid normalized string (ext)', function (done) {
 
                 expect(Hawk.crypto.generateNormalizedString('header', {
-                    credentials: {
-                        key: 'dasdfasdf',
-                        algorithm: 'sha256'
-                    },
                     ts: 1357747017,
                     nonce: 'k3k4j5',
                     method: 'GET',
@@ -64,10 +56,6 @@ describe('Hawk', function () {
             it('should return a valid normalized string (payload + ext)', function (done) {
 
                 expect(Hawk.crypto.generateNormalizedString('header', {
-                    credentials: {
-                        key: 'dasdfasdf',
-                        algorithm: 'sha256'
-                    },
                     ts: 1357747017,
                     nonce: 'k3k4j5',
                     method: 'GET',
